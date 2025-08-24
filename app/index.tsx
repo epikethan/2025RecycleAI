1830ceabad1e28292b018f747611da394c775ee2
Ethan Park <ethanpark1177@gmail.com>
2025-08-23 18:41:01 -0700

    Fixing Emulator

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Link, Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
-export default function Index() {
+export default function NotFoundScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <Stack.Screen  options={{ title: "Page Not Found."}} />
+      <View style = {styles.container}>
+        <Link href={"https://www.carboncare.org/en/co2-emissions-calculator"} style={styles.button}>
+          Go Back to Home Screen
+        </Link>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "white",
+  },
+  button: {
+    fontSize: 20,
+    color: "white",
+  },
+});
