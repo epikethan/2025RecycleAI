8ec9c04f13dd83e663ecb1eeeb04cee16c05a72b
Ethan Park <ethanpark1177@gmail.com>
2025-10-26 17:57:18 -0700

    Updating assets and rewards

--- app/(tabs)/Rewards.tsx
+++ app/(tabs)/Rewards.tsx
@@ -1,3 +1,4 @@
+import { Image } from "expo-image";
 import { StyleSheet, Text, View } from "react-native";
 import { storage } from "../storage";
 
@@ -7,6 +8,42 @@ export default function RewardScreen() {
   return (
     <View style = {styles.container}>
       <Text style={styles.text}>Total Points: </Text>
+      <View style={styles.row}>
+        <Image style={styles.image}
+          source={require("../../assets/images/hat.png")}
+          contentFit="contain"
+          transition={1000}
+        />
+        <Image style={styles.image}
+          source={require("../../assets/images/golf.png")}
+          contentFit="contain"
+          transition={1000}
+        />
+      </View>
+      <View style={styles.row}>
+        <Image style={styles.image}
+          source={require("../../assets/images/mouse_pad.png")}
+          contentFit="contain"
+          transition={1000}
+        />
+        <Image style={styles.image}
+          source={require("../../assets/images/mug.png")}
+          contentFit="contain"
+          transition={1000}
+        />
+      </View>
+      <View style={styles.row}>
+        <Image style={styles.image}
+          source={require("../../assets/images/notebook.png")}
+          contentFit="contain"
+          transition={1000}
+        />
+        <Image style={styles.image}
+          source={require("../../assets/images/water_bottle.png")}
+          contentFit="contain"
+          transition={1000}
+        />
+      </View>
     </View>
   );
 }
@@ -23,4 +60,16 @@ const styles = StyleSheet.create({
   text: {
     color: "white",
   },
+  row: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around", // or 'space-between'
+    alignItems: "center",
+    paddingHorizontal: 12,
+  },
+  image: {
+    width: 150,
+    aspectRatio: 1,
+    backgroundColor: "#ffffff",
+  }
 });
